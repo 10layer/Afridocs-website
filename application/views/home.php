618d2ba1cd06720951d762eee6280241b75c005d
j-norwood-young <jason@10layer.com>
2012-04-03 17:25:34 +0200

    Major checkin

---
+++ application/views/home.php
@@ -0,0 +1,63 @@
+<?php
+	$this->load->view("templates/header");
+?>
+<div class="section">
+<div class="row">
+<header>
+	<span class="span8" id="header">
+		<h1>Welcome to Afridocs</h1>
+		<p class="blurb">Protecting free speech on the mother continent</p>
+	</span>
+</header>
+</div>
+</div>
+<div class="section">
+	<div class="page-header">
+		<h2>What's Afridocs?</h2>
+	</div>
+	<div class="row">
+		<span class="span3">
+			<h2>Communication</h2>
+			<p>Afridocs will offer technology for sources and the media to communicate securely and safely.</p>
+		</span>
+		<span class="span3">
+			<h2>Documents</h2>
+			<p>We'll store, categorise and publish documents that are in the public interest</p>
+		</span>
+		<span class="span3">
+			<h2>Storage</h2>
+			<p>We'll provide safe, confidential storage for media companies to save sensitive information and source documents that they can't keep on their own networks.</p>
+		</span>
+		<span class="span3">
+			<h2>Education</h2>
+			<p>We will produce literature, courses and other information for journalists on the technical aspects of online communication.</p>
+		</span>
+	</div>
+</div>
+<div class="section">
+	<div class="page-header">
+		<h2>Who's Afridocs?</h2>
+	</div>
+	<div class="row">
+		<span class="span3">
+			<h3>Jason Norwood-Young</h3>
+			<p>Jason is a reformed journalist who now builds the technology behind online news publishers through his company, 10Layer.</p>
+		</span>
+		<span class="span3 offset1">
+			<h3>Guy Taylor</h3>
+			<p>Guy is the business brain behind 10Layer, cage fighter and internet libertarian.</p>
+		</span>
+		<span class="span3 offset1">
+			<h3>Others...</h3>
+			<p>Afridocs is supported by numerous journalists and editors who prefer to remain nameless for now.</p>
+		</span>
+	</div>
+	<div class="row">
+		
+		
+		
+	</div>
+</div>
+<?php
+	$this->load->view("templates/footer");
+?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Afridocs</title>
+	<link href="/resources/less/main.css" rel="stylesheet" />
+	<link href="/resources/css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+    	<a class="brand" href="#">
+			Afridocs
+		</a>
+    </div>
+   </div>
+</div>
+<div class="container">
